4086c10d5f53eaaa5e33ab0da3a18cec0636b79e
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-06-12 08:24:29 -0500

    files add

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/service-worker.js')
-//   .then(function(registration) {
-//     console.log('Registration successful, scope is:', registration.scope);
-//   })
-//   .catch(function(error) {
-//     console.log('Service worker registration failed, error:', error);
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
 
 let restaurants,
   neighborhoods,
